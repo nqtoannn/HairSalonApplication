79a426b0e8cafc791d6bc8debdefd9cc2e82b1e4
ngocthong37 <96676595+ngocthong37@users.noreply.github.com>
2024-05-02 20:01:57 +0700

    ...

--- app/src/main/java/com/example/hairsalon/activity/order/OrderDetail.java
+++ app/src/main/java/com/example/hairsalon/activity/order/OrderDetail.java
@@ -2,15 +2,33 @@ package com.example.hairsalon.activity.order;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.hairsalon.R;
+import com.example.hairsalon.databinding.ActivityOrderDetailBinding;
+import com.example.hairsalon.model.User;
 
 public class OrderDetail extends AppCompatActivity {
+
+    ActivityOrderDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_detail);
+        binding = ActivityOrderDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Intent intent = getIntent();
+        if (intent != null) {
+            String nameProduct = intent.getStringExtra("nameProduct");
+            double price = intent.getDoubleExtra("price", 0.0);
+            int quantity = intent.getIntExtra("quantity", 0);
+            String address = intent.getStringExtra("address");
+            String imageUrl = intent.getStringExtra("imageUrl");
+
+        }
+
+
+
     }
 }
