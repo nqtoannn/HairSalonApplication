8ffe14712bcc21c439790021ebd0b3edf87fa063
PKTinh107 <87923370+PKTinh107@users.noreply.github.com>
2024-05-15 22:40:28 +0700

    done pdf

--- app/src/main/java/com/example/hairsalon/activity/statistics/SanPham.java
+++ app/src/main/java/com/example/hairsalon/activity/statistics/SanPham.java
@@ -1,7 +1,14 @@
 package com.example.hairsalon.activity.statistics;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +19,23 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.hairsalon.R;
 import com.example.hairsalon.api.ApiService;
 import com.example.hairsalon.model.ResponseData;
+import com.example.hairsalon.model.User;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -45,6 +62,14 @@ public class SanPham extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnExportPDFSP = findViewById(R.id.btnExportPDFSP);
+        btnExportPDFSP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exportChartToPdf();
+            }
         });
 
         getRevenueFromService();
@@ -119,5 +144,103 @@ public class SanPham extends AppCompatActivity {
         PieData data = new PieData(dataSet);
         pieChart.setData(data);
         pieChart.invalidate(); // Vẽ lại biểu đồ để hiển thị dữ liệu mới
+    }
+
+    private void exportChartToPdf() {
+        // Tạo một Bitmap từ PieChart
+        Bitmap chartBitmap = pieChart.getChartBitmap();
+        final String fullName ;
+
+        if (chartBitmap != null) {
+            // Lưu bitmap thành tệp PDF
+            int scaledWidth = chartBitmap.getWidth() / 2; // Giảm kích thước chiều rộng đi 50%
+            int scaledHeight = chartBitmap.getHeight() / 2; // Giảm kích thước chiều cao đi 50%
+
+            // Tạo một Bitmap mới với kích thước đã được giảm
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(chartBitmap, scaledWidth, scaledHeight, true);
+            try {
+                // Tạo tệp PDF trong thư mục Documents trên bộ nhớ ngoài
+
+                File pdfFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "chart.pdf");
+                FileOutputStream outputStream = new FileOutputStream(pdfFile);
+
+                // Tạo Document của iTextPDF và PdfWriter để ghi vào tệp PDF
+                com.itextpdf.text.Document document = new com.itextpdf.text.Document();
+                PdfWriter.getInstance(document, outputStream);
+
+
+                // Chuyển đổi Bitmap thành Image của iTextPDF
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                scaledBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+                Image image = Image.getInstance(byteArray);
+
+                // Thêm ngày xuất file là ngày hiện tại vào tài liệu PDF
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                String currentDate = sdf.format(new Date());
+
+                // Thêm nội dung văn bản vào tài liệu PDF
+
+                SharedPreferences sharedPreferences = getSharedPreferences("User", Context.MODE_PRIVATE);
+                Integer customerId = sharedPreferences.getInt("userId", -1);
+
+                ApiService.apiService.getCustomerById(customerId).enqueue(new Callback<ResponseData>() {
+                    @Override
+                    public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            ResponseData responseData = response.body();
+                            List<Map<String, Object>> customers = responseData.getData();
+
+                            if (customers != null && !customers.isEmpty()) {
+                                // Lấy thông tin của khách hàng đầu tiên (nếu có)
+                                Map<String, Object> customer = customers.get(0);
+                                String name = (String) customer.get("fullName");
+                                Log.d("Customer FullName", "Full Name: " + name);
+                                try {
+                                    document.open();
+                                    document.add(new com.itextpdf.text.Paragraph("--------------"));
+                                    document.add(new com.itextpdf.text.Paragraph("Thong ke ve so sanh doanh thu giua San pham và Dich vu"));
+                                    document.add(new Paragraph("Nhan vien: " + name));
+                                    document.add(new com.itextpdf.text.Paragraph("Ngay xuat file: " + currentDate));
+                                    document.add(new com.itextpdf.text.Paragraph("--------------"));
+                                    document.add(image);
+                                    document.close();
+
+                                    Toast.makeText(SanPham.this,"PDF exported successfull", Toast.LENGTH_LONG);
+                                } catch (DocumentException e) {
+                                    Log.e("abcd error",""+ e);
+                                    throw new RuntimeException(e);
+                                }
+                            } else {
+                                Log.d("Customer Info", "No customer data found");
+                            }
+                        } else {
+                            Log.d("API Error", "Failed to get customer data");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseData> call, Throwable t) {
+
+                    }
+                });
+
+
+
+//                Log.i("PDF successfully", "PDF exported successfully");
+//                Toast.makeText(this, "PDF exported successfully", Toast.LENGTH_SHORT).show();
+//
+//                // Log đường dẫn đến tệp PDF đã lưu
+//                Log.d("File Path PDF", pdfFile.getAbsolutePath());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e("PDF Error", "Error exporting PDF: " + e.getMessage());
+                Toast.makeText(this, "Error exporting PDF", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Log.e("PDF Error", "Chart bitmap is null, unable to export PDF");
+            Toast.makeText(this, "Unable to export PDF", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/hairsalon/constants/Constant.java
+++ app/src/main/java/com/example/hairsalon/constants/Constant.java
@@ -3,5 +3,5 @@ package com.example.hairsalon.constants;
 
 
 public class    Constant {
-    public static final String baseUrl = "http://192.168.2.29:8080/api/v1/";
+    public static final String baseUrl = "http://192.168.1.12:8080/api/v1/";
 }
